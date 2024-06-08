0ff6d0affd8f940ce8e81ae7c5356275080c5ff1
blocsu <dotsenko.sar@gmail.com>
2024-06-08 08:40:09 +0400

    Date base PostgreSQL + Prisma

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,6 +1,10 @@
+import { prisma } from '../prisma.js'
+
 // @desc Auth user
 // @route POST /api/auth/login
+
 // @access Public
 export const authUser = async (req, res) => {
-    res.body.json({message: 'You are authenticated'});
-};
+	const user = await prisma.user.findMany()
+	res.json(user)
+}
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import 'colors'
+import dotenv from 'dotenv'
 import express from 'express'
+import morgan from 'morgan'
 import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -8,15 +13,27 @@ async function main() {
 	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
-	app.use('/app/auth', authRoutes)
+	app.use('/api/auth', authRoutes)
+	app.get('/test', (req, res) => {
+		res.send('route test')
+	})
 
 	const PORT = process.env.PORT || 5000
 
 	app.listen(
 		PORT,
 		console.log(
-			`Server running is ${process.env.NODE_ENV} mode om port ${PORT}`.green
-				.bold
+			`Server running is ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
 		)
 	)
 }
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.log(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
